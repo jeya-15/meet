4713d696f6356493b36fec1b57b62719d1c760d8
jeya-15 <athi68423@gmail.com>
2025-02-07 23:05:37 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { Pool } = require("pg");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
